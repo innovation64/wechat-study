ebe59af0380598c8e5609b1da91a891253d7a8a4
innovation64 <45715979+innovation64@users.noreply.github.com>
2020-10-17 22:05:03 +0800

    Add files via upload

---
+++ audio_playly/utils/data.js
@@ -0,0 +1,32 @@
+export default [{
+    src: 'https://s320.xiami.net/690/2110200690/2104936450/1811837759_1560087366632_1258.mp3?ccode=xiami_web_web&expire=86400&duration=216&psid=8a177f9e0cced877bf57112f25da1184&ups_client_netip=124.207.31.241&ups_ts=1602942148&ups_userid=0&utid=uZYIGHtDxEkCAXzPHtJ0ZHLo&vid=1811837759&fn=1811837759_1560087366632_1258.mp3&vkey=B1f4f6068000addad71d129c4a7f7556a',
+    poster: 'https://pic.xiami.net/images/album/img79/5/5e8d647c57503_271579_1586324604.jpg?x-oss-process=image/quality,q_80/format,jpg',
+    name: '芒种',
+    author: '赵方婧'
+  },
+  {
+    src: 'https://s128.xiami.net/480/2099991480/2102928811/1797640766_1510601350739.mp3?ccode=xiami_web_web&expire=86400&duration=216&psid=c2709ff96647296014806bb5bb642c66&ups_client_netip=124.207.31.241&ups_ts=1602942690&ups_userid=0&utid=uZYIGHtDxEkCAXzPHtJ0ZHLo&vid=1797640766&fn=1797640766_1510601350739.mp3&vkey=Bfe7b901223906f6dc35cc3f582f4c671',
+    poster: 'https://pic.xiami.net/images/album/img80/2099991480/6013481510601349.jpg?x-oss-process=image/quality,q_80/format,jpg',
+    name: '生僻字',
+    author: '陈柯宇'
+  },
+  {
+    src: 'https://s320.xiami.net/7/706276007/2100340412/1776099904_1587711019743_1383.mp3?ccode=xiami_web_web&expire=86400&duration=313&psid=14188726a1a82cda1e550e09751f99fe&ups_client_netip=124.207.31.241&ups_ts=1602942829&ups_userid=0&utid=uZYIGHtDxEkCAXzPHtJ0ZHLo&vid=1776099904&fn=1776099904_1587711019743_1383.mp3&vkey=Beac24b2e10b66190a26f80b5423a4f8a',
+    poster: 'https://pic.xiami.net/images/album/img7/706276007/21003404121463658099.png?x-oss-process=image/quality,q_80/format,jpg',
+    name: '大鱼',
+    author: '周深'
+  },
+  {
+    src: 'https://s128.xiami.net/692/2110151692/2103560586/1802137052_1521531845596.mp3?ccode=xiami_web_web&expire=86400&duration=248&psid=343f5e19612e89bdbae81116c9442ec3&ups_client_netip=124.207.31.241&ups_ts=1602942906&ups_userid=0&utid=uZYIGHtDxEkCAXzPHtJ0ZHLo&vid=1802137052&fn=1802137052_1521531845596.mp3&vkey=Bd36b7cd354bbdfcc1cb521be153174fe',
+    poster: 'https://pic.xiami.net/images/album/img82/59382/21035605861525059383.jpg?x-oss-process=image/quality,q_80/format,jpg',
+    name: '离人愁',
+    author: '李袁杰'
+  },
+  {
+    src: 'https://s128.xiami.net/931/272044931/1623630285/1773985965_1587810990218_2161.mp3?ccode=xiami_web_web&expire=86400&duration=239&psid=268122660e12ef65f352244ce8696fce&ups_client_netip=124.207.31.241&ups_ts=1602943000&ups_userid=0&utid=uZYIGHtDxEkCAXzPHtJ0ZHLo&vid=1773985965&fn=1773985965_1587810990218_2161.mp3&vkey=Bdd86e011fd85081c36356d79e689f092',
+    poster: 'https://pic.xiami.net/images/album/img31/272044931/16236302851468230932.jpg?x-oss-process=image/quality,q_80/format,jpg',
+    name: '牵丝戏',
+    author: "银临",
+  }
+  
+]
